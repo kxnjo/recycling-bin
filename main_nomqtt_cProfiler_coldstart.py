41b29fddb55c2d6aba0a1f185bd4291ebeaaddd4
kxnjo <84301894+kxnjo@users.noreply.github.com>
2026-03-26 18:37:35 +0800

    add delayed close into coldstart

--- main_nomqtt_cProfiler_coldstart.py
+++ main_nomqtt_cProfiler_coldstart.py
@@ -5,7 +5,7 @@ import sys
 import config, cProfile, pstats, io
 # If your file really lives in profiling/hardware_wProfiling.py, switch this import back.
 import hardware_wProfiling as hardware_wProfiling
-import ai_vision as ai_vision
+import ai_vision_delayedClose as ai_vision
 from profiler import log_profile, now, profile_block
 
 capture_event = Event()
